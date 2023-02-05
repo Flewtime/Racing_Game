b91648f25d0113a3d5ca37df432322812f98441b
Jonathan Vancent <Jonathanvancent.jK@gmail.com>
2023-02-05 07:48:05 +0700

    Pause & Build
    Pause & Build

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -9,7 +9,10 @@ public class UIManager : MonoBehaviour
 
     public TMP_Text lapCounterText, bestLapTimeText, CurrLapTimeText, positionText, countdownText, goText, raceResultText;
 
+    public GameObject pauseScreen;
     public GameObject resultsScreen;
+
+    public bool isPaused;
     private void Awake() {
         instance = this;
     }
@@ -21,11 +24,33 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            pauseUnPause();
+        }   
     }
-    
+
+
+
+    public void pauseUnPause(){
+        isPaused = !isPaused;
+        pauseScreen.SetActive(isPaused);
+
+        if(isPaused){
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
+    }
+
+
     public void ExitRace()
     {
+        Time.timeScale = 1f;
         RaceManager.instance.ExitRace();
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("Game Quit");
     }
 }
